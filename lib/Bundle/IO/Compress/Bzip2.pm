834f3ef1f51952c736301afada36b11a25095639
Paul Marquess <pmqs@cpan.org>
2011-01-06 11:22:06 +0000

    Import of PMQS/Bundle-IO-Compress-Bzip2-2.032 from CPAN.
    gitpan-cpan-distribution: Bundle-IO-Compress-Bzip2
    gitpan-cpan-version:      2.032
    gitpan-cpan-path:         PMQS/Bundle-IO-Compress-Bzip2-2.032.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/Bundle/IO/Compress/Bzip2.pm
+++ lib/Bundle/IO/Compress/Bzip2.pm
@@ -2,7 +2,7 @@ package Bundle::IO::Compress::Bzip2;
 
 use strict;
 
-$Bundle::IO::Compress::Bzip2::VERSION = '2.030';
+$Bundle::IO::Compress::Bzip2::VERSION = '2.032';
 
 1;
 __END__
@@ -19,11 +19,11 @@ Bundle::IO::Compress::Bzip2 - Install IO::Compress::Bzip2 and dependencies
 
 Scalar::Util
 
-Compress::Raw::Bzip2 2.030
+Compress::Raw::Bzip2 2.032
 
-Compress::Raw::Zlib 2.030
+Compress::Raw::Zlib 2.032
 
-IO::Compress::Bzip2 2.030
+IO::Compress::Bzip2 2.032
 
 =head1 AUTHOR
 
@@ -35,7 +35,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2008-2010 Paul Marquess. All rights reserved.
+Copyright (c) 2008-2011 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.

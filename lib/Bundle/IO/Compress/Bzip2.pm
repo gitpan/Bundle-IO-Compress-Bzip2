72b7a85112cc8bf38d82e4cb32be6f72e534e452
Paul Marquess <pmqs@cpan.org>
2014-12-23 17:44:22 +0000

    Import of PMQS/Bundle-IO-Compress-Bzip2-2.068.tar.gz from CPAN.
    gitpan-cpan-distribution: Bundle-IO-Compress-Bzip2
    gitpan-cpan-version:      2.068
    gitpan-cpan-path:         PMQS/Bundle-IO-Compress-Bzip2-2.068.tar.gz
    gitpan-cpan-author:       PMQS
    gitpan-cpan-maturity:     released

--- lib/Bundle/IO/Compress/Bzip2.pm
+++ lib/Bundle/IO/Compress/Bzip2.pm
@@ -2,7 +2,7 @@ package Bundle::IO::Compress::Bzip2;
 
 use strict;
 
-$Bundle::IO::Compress::Bzip2::VERSION = '2.067';
+$Bundle::IO::Compress::Bzip2::VERSION = '2.068';
 
 1;
 __END__
@@ -19,11 +19,11 @@ Bundle::IO::Compress::Bzip2 - Install IO::Compress::Bzip2 and dependencies
 
 Scalar::Util
 
-Compress::Raw::Bzip2 2.067
+Compress::Raw::Bzip2 2.068
 
-Compress::Raw::Zlib 2.067
+Compress::Raw::Zlib 2.068
 
-IO::Compress::Bzip2 2.067
+IO::Compress::Bzip2 2.068
 
 =head1 AUTHOR
 
